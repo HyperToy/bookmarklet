f3e50d72beb7d1731151f8533a0b439deab38394
NODA Ryohei <ryohey.trumpet@gmail.com>
2025-12-28 10:53:25 +0900

    add paste_link_to_today_page

---
+++ src/paste_link_to_today_page.js
@@ -0,0 +1,37 @@
+(() => {
+	const PROJECT_NAME = "PROJECT_NAME";
+	const domain = window.location.hostname;
+	const isScrapboxDomain = domain.toLowerCase().endsWith("scrapbox.io");
+	const isTwitterDomain = () => domain.toLowerCase().endsWith("x.com");
+	const twitterUrl = (defaultText) => {
+		try {
+			return [
+				">",
+				document.title.match(/「(.*)」/)[1],
+				`(${document.title.match(/Xユーザーの(.*)さん: 「/)[1]}`,
+				`[@${document.URL.match(/x.com\/([^\/]*)/)[1]} ${document.URL}])`,
+			].join(" ");
+		} catch (e) {
+			return defaultText;
+		}
+	};
+	const url = window.location.href;
+	const title = document.title
+		.replace("[", "")
+		.replace("]", "")
+		.replaceAll("`", " ");
+	const body = encodeURIComponent(
+		isScrapboxDomain
+			? (() => {
+					const regexp = new RegExp(`(.*) - ${PROJECT_NAME}`);
+					return `[${document.title.match(regexp)[1]}]`;
+				})()
+			: isTwitterDomain()
+				? twitterUrl(`[${title} ${url}]`)
+				: `[${title} ${url}]`,
+	);
+	const pageTitle = new Date().toISOString().split("T")[0];
+	window.open(
+		`https://scrapbox.io/${PROJECT_NAME}/${encodeURIComponent(pageTitle.trim())}?body=${body}`,
+	);
+})();
